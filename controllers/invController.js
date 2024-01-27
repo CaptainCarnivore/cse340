2894d3e0c153b83adf7affface033cf35dfedd52
CaptainCarnivore <ausjensen0451@byui.edu>
2024-01-27 16:35:05 -0700

    fixed some things

--- controllers/invController.js
+++ controllers/invController.js
@@ -10,6 +10,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
   console.log(classification_id)
   const data = await invModel.getInventoryByClassificationId(classification_id)
+  if (data.length > 0) {
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
   const className = data[0].classification_name
@@ -18,7 +19,11 @@ invCont.buildByClassificationId = async function (req, res, next) {
     nav,
     grid,
   })
-}
+} else {
+  const err = new Error("Sorry, we appear to have lost that page.");
+  err.status = 404;
+  next(err);
+}}
 
 /* ***************************
  *  Build inventory item detail by inv id
@@ -26,6 +31,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
 invCont.buildByInventoryId = async function(req, res, next) {
   const inventory_id = req.params.invId
   const data = await invModel.getItemByInventoryId(inventory_id)
+  if (data.length > 0) {
   const detail = await utilities.buildItemDetailView(data[0])
   let nav = await utilities.getNav()
   const className = data[0].inv_year + " " + data[0].inv_make + " " + data[0].inv_model
@@ -34,7 +40,10 @@ invCont.buildByInventoryId = async function(req, res, next) {
     nav,
     detail,
   })
-}
-
+} else {
+  const err = new Error("Sorry, we appear to have lost that page.");
+  err.status = 404;
+  next(err);
+}}
 
 module.exports = invCont
